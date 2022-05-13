7a8f899fee7fa8a265ac8193c1de0236b7f989b8
yongzx <zheng_xin_yong@brown.edu>
2022-05-12 21:28:24 -0400

    use updated eval_xnli/adapters_xnli_de_vn.py

--- scripts/eval_xnli/adapters_xnli_de.py
+++ scripts/eval_xnli/adapters_xnli_de.py
@@ -27,18 +27,18 @@ parser.add_argument("--num_train_epochs", type=int, default=30)
 parser.add_argument("--learning_rate", type=float, default=1e-5)
 parser.add_argument("--per_device_train_batch_size", type=int, default=4)
 parser.add_argument("--gradient_accumulation_steps", type=int, default=4)
-parser.add_argument("--pretrained_model") 
+parser.add_argument("--adapted_model") 
 parser.add_argument("--original_model")  
 parser.add_argument("--tokenizer")
 parser.add_argument("--do_train", default=False, action="store_true")
 parser.add_argument("--do_eval_after_train", default=False, action="store_true")
 parser.add_argument("--do_predict", default=False, action="store_true")
 parser.add_argument("--use_partial_data", default=False, action="store_true")
-parser.add_argument("--zero_shot", default=False, action="store_true")
+parser.add_argument("--cross_lingual", default=False, action="store_true")
 
 finetune_strategies = ["whole", "lang_adapters", "task_adapters"]
 parser.add_argument("--madx_lang_adapter")
-parser.add_argument("--adapter_lang_name", required=True)
+#parser.add_argument("--adapter_lang_name", required=True) -- why is this required??
 parser.add_argument("--finetune_strategies", choices=finetune_strategies, required=True)
 
 args = parser.parse_args()
@@ -46,21 +46,20 @@ if args.do_eval_after_train:
     args.do_predict = True
 
 if args.original_model is None:
-    # here: because the wpe is not saved, pretrained_model is the original bigsciece model
-    args.original_model = args.pretrained_model
+    # here: because the wpe is not saved, adapted_model is the original bigsciece model
+    args.original_model = args.adapted_model
 
 print("Arguments: ========")
 print(args)
 
 
 # load dataset
-if args.zero_shot:
+if args.cross_lingual:
     print("0️⃣ 0-Shot")
     # 0-shot: use english as train and validation
     xnli_en_dataset = load_dataset("xnli", "en", cache_dir=args.cache_dir)
     xnli_dataset = load_dataset("xnli", args.lang, cache_dir=args.cache_dir)
     assert args.lang != "en"
-
     train_dataset = xnli_en_dataset['train']
     val_dataset = xnli_en_dataset['validation']
     test_dataset = xnli_dataset['test']
@@ -76,7 +75,7 @@ else:
 # load tokenizer
 tokenizer = AutoTokenizer.from_pretrained(args.tokenizer, cache_dir=args.cache_dir)
 tokenizer.pad_token = tokenizer.eos_token  # tokenizer.encode(tokenizer.eos_token) = [0]
-if args.zero_shot:
+if args.cross_lingual:
     en_tokenizer = AutoTokenizer.from_pretrained(args.original_model, cache_dir=args.cache_dir) # has to use AutoTokenizer instead of GPT2Tokenizer
     en_tokenizer.pad_token = en_tokenizer.eos_token
 
@@ -88,20 +87,22 @@ def en_tokenize_function(examples):
 
 
 logger.info("Tokenizing the dataset...")
-if args.zero_shot:
-    full_train_dataset = train_dataset.map(en_tokenize_function, batched=False)
-    full_val_dataset = val_dataset.map(en_tokenize_function, batched=False)
-else:
-    full_train_dataset = train_dataset.map(tokenize_function, batched=False)
-    full_val_dataset = val_dataset.map(tokenize_function, batched=False)
+if args.do_train:
+    if args.cross_lingual:
+        full_train_dataset = train_dataset.map(en_tokenize_function, batched=False)
+        full_val_dataset = val_dataset.map(en_tokenize_function, batched=False)
+    else:
+        full_train_dataset = train_dataset.map(tokenize_function, batched=False)
+        full_val_dataset = val_dataset.map(tokenize_function, batched=False)
+
+
+    small_train_dataset = full_train_dataset.shuffle(seed=42).select(range(100))
+    small_val_dataset = full_val_dataset.shuffle(seed=42).select(range(100))
+    logger.info(full_train_dataset[0])
+    logger.info(full_train_dataset[100])
 
 full_test_dataset = test_dataset.map(tokenize_function, batched=False)
-small_train_dataset = full_train_dataset.shuffle(seed=42).select(range(100))
-small_val_dataset = full_val_dataset.shuffle(seed=42).select(range(100))
 small_test_dataset = full_test_dataset.shuffle(seed=42).select(range(100))
-
-logger.info(full_train_dataset[0])
-logger.info(full_train_dataset[100])
 
 from datasets import load_metric
 metric = load_metric("xnli")
@@ -132,51 +133,40 @@ training_args = TrainingArguments(
 )
 
 def load_model(args, inference=False):
-
     # FIXME: if we load with GPT2ForSequenceClassification, the embeddings are the original one 
     # even when we call load_adapter
-    if args.zero_shot and not inference:
-        model = GPT2ForSequenceClassification.from_pretrained(args.pretrained_model, 
-                                                            num_labels=3,
-                                                            pad_token_id=en_tokenizer.pad_token_id,
-                                                            cache_dir=args.cache_dir)
-    else:
-        model = GPT2ForSequenceClassification.from_pretrained(args.pretrained_model, 
-                                                            num_labels=3,
-                                                            pad_token_id=tokenizer.pad_token_id,
-                                                            cache_dir=args.cache_dir)
-
-    if not args.zero_shot or (args.zero_shot and inference):
-        # if not zero shot, that means that we need to replace the embedding layers during training
-        # we also need to replace embedding layers during inference
-        causal_lm_model = AutoModelForCausalLM.from_pretrained(args.original_model)
-
-        # change the embedding layer of the original big science model
-        # by loading the adapters (which has saved lm_head)
-        causal_lm_model.resize_token_embeddings(len(tokenizer))
-        if args.madx_lang_adapter:
-            causal_lm_model.load_adapter(args.madx_lang_adapter, config="pfeiffer+inv")                                    
+    if not args.original_model == args.adapted_model and not args.cross_lingual:
+        wte = torch.load(f'{args.adapted_model}/embedding.pt')
+        wpe = torch.load(f'{args.adapted_model}/positional_embedding.pt')        
         
-        # model has original bigscience embedding so replace it.
-        model.resize_token_embeddings(len(tokenizer))
-        model._modules['transformer']._modules['wte'] = causal_lm_model._modules['transformer']._modules['wte']
+    model = GPT2ForSequenceClassification.from_pretrained(args.original_model, 
+                                                          num_labels=3,
+                                                          pad_token_id=en_tokenizer.pad_token_id,
+                                                          cache_dir=args.cache_dir)
+
+    if inference or not args.cross_lingual:
+        # need to load embedding/adapters from the model adapted to the new language
+        causal_lm_model = AutoModelForCausalLM.from_pretrained(args.original_model)
+        causal_lm_model.resize_token_embeddings(len(tokenizer))
+        if not args.original_model == args.adapted_model:
+            causal_lm_model.transformer.wte = wte
+            causal_lm_model.transformer.wpe = wpe
+        if args.madx_lang_adapter:
+            adapter_name = causal_lm_model.load_adapter(args.madx_lang_adapter, config="pfeiffer+inv")                                    
+            model.transformer = causal_lm_model.transformer
+            model.set_active_adapters(adapter_name)
 
     if not inference:
-        if not args.zero_shot:
-            if args.madx_lang_adapter:
-                adapter_name = model.load_adapter(args.madx_lang_adapter,
-                                                config="pfeiffer+inv",
-                                                load_as=args.adapter_lang_name)
-        if args.finetune_strategies == "whole":
-            model.set_active_adapters(adapter_name)
-        elif args.finetune_strategies == "lang_adapters":
-            model.train_adapter([args.adapter_lang_name])
-        elif args.finetune_strategies == "task_adapters":
-            model.add_adapter("xnli-task-adapter")
-            model.train_adapter("xnli-task-adapter")
-        else:
-            raise ValueError("Lack configuration")
-        
+        #if not args.cross_lingual: normally need to add adapter in any case
+            # normally this is already done, why use adapter_lang_name here?
+            #if args.madx_lang_adapter:
+            #    adapter_name = model.load_adapter(args.madx_lang_adapter,
+            #                                    config="pfeiffer+inv",
+            #                                    load_as=args.adapter_lang_name)
+        model.add_adapter("xnli-task-adapter")
+        model.train_adapter("xnli-task-adapter")
+
+
         print("🔥 ==================== Training: ==================== 🔥")
         for name, param in model.named_parameters():
             if not param.requires_grad:
@@ -185,24 +175,19 @@ def load_model(args, inference=False):
                 print(f"🚀 Trainable layer '{name}'")
         print(model)
     else:
-        print("🔥 ==================== Inference: ==================== 🔥")
-        if args.finetune_strategies == "lang_adapters":
-            assert args.pretrained_adapters_dir 
-            adapter_name = model.load_adapter(f"{args.pretrained_adapters_dir}/{args.adapter_lang_name}")
-            model.set_active_adapters(adapter_name)
-        elif args.finetune_strategies == "task_adapters":
-            if args.madx_lang_adapter:
-                assert args.pretrained_adapters_dir 
-                adapter_name = model.load_adapter(args.madx_lang_adapter)
-                model.set_active_adapters(adapter_name)
-                adapter_name = model.load_adapter(f"{args.pretrained_adapters_dir}/xnli-task-adapter")
-                model.set_active_adapters(adapter_name)
-            else:
-                # adapter_name = model.load_adapter("/users/zyong2/data/zyong2/bigscience/data/processed/013/xnli_de_de_100K_adpt_16_0shot/checkpoint-24544/xnli-task-adapter")
-
-                # for TGT -> TGT supervised finetuning setting, change adapter_name
-                adapter_name = model.load_adapter("/users/zyong2/data/zyong2/bigscience/data/processed/exp-013/task_xnli_de_ft_100000_ori/checkpoint-24544/xnli-task-adapter")
-                model.set_active_adapters(adapter_name)
+        #if args.madx_lang_adapter:
+        assert args.pretrained_adapters_dir 
+            # normally this is done in any case
+            #adapter_name = model.load_adapter(args.madx_lang_adapter)
+            #model.set_active_adapters(adapter_name)
+        adapter_name = model.load_adapter(f"{args.pretrained_adapters_dir}/xnli-task-adapter")
+        model.set_active_adapters(adapter_name)
+        #else:
+        #        # adapter_name = model.load_adapter("/users/zyong2/data/zyong2/bigscience/data/processed/013/xnli_de_de_100K_adpt_16_0shot/checkpoint-24544/xnli-task-adapter")
+        #        # not sure what happens here
+        #        # for TGT -> TGT supervised finetuning setting, change adapter_name
+        #        adapter_name = model.load_adapter("/users/zyong2/data/zyong2/bigscience/data/processed/exp-013/task_xnli_de_ft_100000_ori/checkpoint-24544/xnli-task-adapter")
+        #        model.set_active_adapters(adapter_name)
         print(model)
 
     return model

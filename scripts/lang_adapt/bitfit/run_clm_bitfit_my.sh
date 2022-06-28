7c2b034786f502423fab62c12b3bc6f0a14e52b4
Lintang Sutawika <lintang@datasaur.ai>
2022-06-28 18:40:14 +0700

    moved bitfit scripts

--- scripts/lang_adapt/bitfit/run_clm_bitfit_my.sh
+++ scripts/lang_adapt/bitfit/run_clm_bitfit_my.sh
@@ -5,12 +5,12 @@ LANG="my"
 DATA_SAMPLES=100000 #$(($SLURM_ARRAY_TASK_ID * 1000))
 VOCAB_SIZE=5000
 CH=118500
-BIGS_MODEL="bigscience/bloom-1b3"
+BIGS_MODEL="bigscience/bloom-350m"
 ADPT_STRATEGY="emb-and-adpt"
-EMBD_SRATEGY="overlap-replace"
+EMBD_SRATEGY="extend"
 FTNE_STRATEGY="bitfit"
 
-tokenizer_dir="bigscience/bloom-1b3" #"/users/zyong2/data/zyong2/bigscience/data/processed/020/tok_${BIGS_MODEL##*/}_${LANG}_oscar_${DATA_SAMPLES}samples_${VOCAB_SIZE}vocab_${EMBD_SRATEGY}"
+tokenizer_dir="checkpoint/tok_bloom-350m_my_oscar_100000samples_24000vocab_extend"
 cache_dir="checkpoint/cache/"
 output_dir="checkpoint/${BIGS_MODEL##*/}_${LANG}_${ADPT_STRATEGY}_${EMBD_SRATEGY}_${FTNE_STRATEGY}_${DATA_SAMPLES}samples"
 logging_dir="checkpoint/${BIGS_MODEL##*/}_${LANG}_${ADPT_STRATEGY}_${EMBD_SRATEGY}_${FTNE_STRATEGY}_${DATA_SAMPLES}samples"
@@ -28,16 +28,17 @@ python scripts/lang_adapt/madx_run_clm.py \
     --dataset_config_name "unshuffled_deduplicated_${LANG}" \
     --logging_dir $logging_dir \
     --report_to "tensorboard" \
-    --learning_rate 0.001 \
+    --learning_rate 1e-4 \
+    --lr_scheduler_type "constant" \
     --do_train \
     --do_eval \
     --output_dir $output_dir \
     --preprocessing_num_workers 8 \
     --overwrite_output_dir \
-    --per_device_train_batch_size 2 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 16 \
     --per_device_eval_batch_size 2 \
-    --eval_accumulation_steps 4 \
+    --eval_accumulation_steps 1 \
     --eval_steps 1000 \
     --evaluation_strategy "epoch" \
     --max_eval_samples 5000 \

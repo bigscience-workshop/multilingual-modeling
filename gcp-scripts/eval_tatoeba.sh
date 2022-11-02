ab6d9610c692febb6511ae09b155384cfaf164e4
yongzx <zheng_xin_yong@brown.edu>
2022-11-02 19:40:14 +0000

    tatoeba all checkpoints

--- gcp-scripts/eval_tatoeba.sh
+++ gcp-scripts/eval_tatoeba.sh
@@ -1,9 +1,9 @@
 language="en-ru"
 
-# model_name="bigscience/bloom-1b7"
-model_name="/home/zhengxinyong/outputs/bloom-7b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-5000"
+# model_name="bigscience/bloom-7b1"
+model_name="/home/zhengxinyong/outputs/bloom-1b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-5000"
 
-base_model="bigscience/bloom-7b1"
+base_model="bigscience/bloom-1b1"
 
 python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
 --cache_dir "/home/zhengxinyong/.cache/huggingface" \
@@ -11,5 +11,49 @@ python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pi
 --model_name $model_name \
 --tokenizer $base_model \
 --base_model $base_model \
---device cuda:0
+--device cuda:6
 
+model_name="/home/zhengxinyong/outputs/bloom-1b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-10000"
+python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
+--cache_dir "/home/zhengxinyong/.cache/huggingface" \
+--lang_pairs $language \
+--model_name $model_name \
+--tokenizer $base_model \
+--base_model $base_model \
+--device cuda:6
+
+model_name="/home/zhengxinyong/outputs/bloom-1b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-15000"
+python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
+--cache_dir "/home/zhengxinyong/.cache/huggingface" \
+--lang_pairs $language \
+--model_name $model_name \
+--tokenizer $base_model \
+--base_model $base_model \
+--device cuda:6
+
+model_name="/home/zhengxinyong/outputs/bloom-1b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-20000"
+python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
+--cache_dir "/home/zhengxinyong/.cache/huggingface" \
+--lang_pairs $language \
+--model_name $model_name \
+--tokenizer $base_model \
+--base_model $base_model \
+--device cuda:6
+
+model_name="/home/zhengxinyong/outputs/bloom-1b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-25000"
+python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
+--cache_dir "/home/zhengxinyong/.cache/huggingface" \
+--lang_pairs $language \
+--model_name $model_name \
+--tokenizer $base_model \
+--base_model $base_model \
+--device cuda:6
+
+model_name="/home/zhengxinyong/outputs/bloom-1b1_ru_continual-pretrain_100000samples_-1vocab_original/"
+python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
+--cache_dir "/home/zhengxinyong/.cache/huggingface" \
+--lang_pairs $language \
+--model_name $model_name \
+--tokenizer $base_model \
+--base_model $base_model \
+--device cuda:6

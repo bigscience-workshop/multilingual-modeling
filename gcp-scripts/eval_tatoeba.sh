30bb86ac2a4ea69d10c4571672ea065f089d0790
yongzx <zheng_xin_yong@brown.edu>
2022-11-02 18:35:28 +0000

    tatoeba

---
+++ gcp-scripts/eval_tatoeba.sh
@@ -0,0 +1,15 @@
+language="en-ru"
+
+# model_name="bigscience/bloom-1b7"
+model_name="/home/zhengxinyong/outputs/bloom-7b1_ru_continual-pretrain_100000samples_-1vocab_original/checkpoint-5000"
+
+base_model="bigscience/bloom-7b1"
+
+python3 /home/zhengxinyong/multilingual-modeling/scripts/eval/scripts_tatoeba/pilot_baseline.py \
+--cache_dir "/home/zhengxinyong/.cache/huggingface" \
+--lang_pairs $language \
+--model_name $model_name \
+--tokenizer $base_model \
+--base_model $base_model \
+--device cuda:0
+

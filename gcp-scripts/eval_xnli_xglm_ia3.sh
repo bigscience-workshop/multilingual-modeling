62e94c1b7803581f107f4d65f9d498cb34e2fc0e
yongzx <zheng_xin_yong@brown.edu>
2023-11-12 23:30:30 -0500

    clean repo

--- gcp-scripts/eval_xnli_xglm_ia3.sh
+++
@@ -1,9 +0,0 @@
-tok="bigscience/bloom-7b1"
-M="/home/zhengxinyong/outputs/bloom-7b1_ru_ia3+inv_100000samples_-1vocab_original-frozen/oscar_ia3+inv_ru"
-
-python3 /home/zhengxinyong/lm-evaluation-harness/main.py \
---model bigscience \
---model_args tokenizer=$tok,pretrained=$tok,adapter_ckpt_folder=$M \
---tasks xnli_ru \
---no_cache \
---device cuda:0
